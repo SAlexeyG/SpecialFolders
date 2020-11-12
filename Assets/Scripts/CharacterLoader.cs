7cf51fe1f7173c2fa0e2b48bc035be6dc1bd03c4
Alexey Smyshlaev <alexeysm@playtika.com>
2020-11-12 18:15:36 +0300

    Task1

---
+++ Assets/Scripts/CharacterLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CartoonHeroes;
+using UnityEngine;
+
+public class CharacterLoader : MonoBehaviour
+{
+    [SerializeField] private Transform prefabRoot;
+    private GameObject obj;
+
+    public void LoadMale()
+    {
+        var prefab = Resources.Load<GameObject>($"Prafabs/Male A");
+        if (obj != null) Destroy(obj);
+        obj = Instantiate(prefab, prefabRoot);
+    }
+
+    public void LoadFemale()
+    {
+        var prefab = Resources.Load<GameObject>($"Prafabs/Female A");
+        if (obj != null) Destroy(obj);
+        obj = Instantiate(prefab, prefabRoot);
+    }
+}
