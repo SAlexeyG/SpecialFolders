99e445229541223e92234586aac7e968da00299f
Alexey Smyshlaev <alexeysm@playtika.com>
2020-11-12 17:24:09 +0300

    Init

---
+++ Assets/Scripts/EffectsConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu]
+public class EffectsConfig : ScriptableObject
+{
+    [SerializeField] private string[] effects;
+
+    public string[] Effects => effects;
+
+    public GameObject GetEffect(string effectName)
+    {
+        var objName = effects.FirstOrDefault(e => e == effectName);
+        return string.IsNullOrEmpty(objName) ? null : LoadObject(effectName);
+    }
+
+    public GameObject GetRandomEffect()
+    {
+        var effectName = effects[Random.Range(0, effects.Length)];
+        return LoadObject(effectName);
+    }
+
+    private static GameObject LoadObject(string effectName)
+    {
+        return Resources.Load<GameObject>($"Effects/{effectName}");
+    }
+
+#if UNITY_EDITOR
+
+    private void Reset()
+    {
+        var objects = Resources.LoadAll<GameObject>("Effects");
+
+        effects = new string[objects.Length];
+
+        for (int i = 0; i < effects.Length; i++)
+        {
+            effects[i] = objects[i].name;
+        }
+    }
+
+#endif
+}
---
+++ Assets/Scripts/StreamingAssetsLoader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class StreamingAssetsLoader : MonoBehaviour
+{
+    [SerializeField] private ImageData baseIcon;
+
+    private void Start()
+    {
+        baseIcon.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space))
+            return;
+
+        baseIcon.gameObject.SetActive(true);
+
+        var directoryInfo = new DirectoryInfo(Application.streamingAssetsPath);
+
+        print($"Streaming path: {Application.streamingAssetsPath}");
+
+        var allFiels = directoryInfo.GetFiles("*.*");
+
+        foreach(var fileInfo in allFiels)
+        {
+            Debug.Log($"File name {fileInfo.Name}");
+
+            if (fileInfo.Name.Contains("meta"))
+                continue;
+
+            var imageData = Instantiate(baseIcon, baseIcon.transform.parent);
+
+            var bytes = File.ReadAllBytes(fileInfo.FullName);
+            var texture2D = new Texture2D(1, 1);
+
+            texture2D.LoadImage(bytes);
+
+            var rect = new Rect(0, 0, texture2D.width, texture2D.height);
+            var pivot = new Vector2(0.5f, 0.5f);
+
+            var sprite = Sprite.Create(texture2D, rect, pivot);
+            imageData.Image.sprite = sprite;
+            imageData.Text.text = fileInfo.Name;
+        }
+
+        baseIcon.gameObject.SetActive(false);
+    }
+}
